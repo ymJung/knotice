8ce90b220e856a6eac1f7c2560faa49dd1dfd6ca
ymJung <metalbird0@gmail.com>
2025-01-20 22:33:18 +0900

    initialize

---
+++ src/main/kotlin/com/ym/knotice/KnoticeApplication.kt
@@ -0,0 +1,11 @@
+package com.ym.knotice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KnoticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KnoticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/ym/knotice/KnoticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ym.knotice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KnoticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
